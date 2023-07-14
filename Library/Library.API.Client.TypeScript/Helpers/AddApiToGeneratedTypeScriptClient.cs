14afaf3b0f08ec9541904f6c6206722ece4df503
mihailo4343 <mvukasinovic96@gmail.com>
2023-07-14 02:02:07 +0200

    Include nswag typesctript client generator

---
+++ Library/Library.API.Client.TypeScript/Helpers/AddApiToGeneratedTypeScriptClient.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using NJsonSchema;
+using NSwag;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+
+namespace Library.API.Client.TypeScript.Helpers
+{
+	[UsedImplicitly]
+	public class AddApiToGeneratedTypeScriptClient : IOperationProcessor
+	{
+		public bool Process(OperationProcessorContext context)
+		{
+			context.OperationDescription.Path = $"/{context.OperationDescription.Path}";
+			
+			return true;
+		}
+	}
+}
